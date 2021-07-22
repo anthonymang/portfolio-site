f14675208f898ec91c4b596c23cef278e528ad4a
anthonymang <anthonymanganielloda@gmail.com>
2021-07-22 13:19:22 -0400

    fix collapse issue

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 function collapse (x) {
     if (x.matches) {
-        let collapsing = document.getElementsByClassName('mobile-coll')
+        let pizza = document.getElementById('collapsePizza')
+        let share = document.getElementById('collapseShare')
+        let gallery = document.getElementById('collapseGallery')
+        let point = document.getElementById('collapsePONR')
+        let collapsing = [pizza, gallery, point, share]
+        console.log(collapsing)
         for (let i=0; i < collapsing.length; i++){
             collapsing[i].className = 'collapse'
+
         }
 
     } else {
         console.log('desktop')
         let collapsing = document.getElementsByClassName('coll-link')
         for (let i=0; i < collapsing.length; i++){
+            console.log(collapsing[i])
             collapsing[i].removeAttribute('href');
             collapsing[i].setAttribute('data-bs-toggle', '');
             collapsing[i].style.cursor = 'inherit';
